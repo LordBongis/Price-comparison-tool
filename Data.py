a93ebea79c2b23043eb9b6796c058358534da72e
LordBongis <134457452+LordBongis@users.noreply.github.com>
2023-09-02 07:59:06 +0000

    Created the Data file, and made the classing and a list to hold the class information

---
+++ Data.py
@@ -0,0 +1,9 @@
+#Class to sort the product information.
+class Product:
+  def product_initialiser(self, name, cost, value):
+    self.name = name
+    self.cost = cost
+    self.value = value
+#List to hold the Product information
+products = []
+
