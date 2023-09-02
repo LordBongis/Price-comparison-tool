c83fab12db07466f46ef01bea7b53832970af505
LordBongis <134457452+LordBongis@users.noreply.github.com>
2023-09-02 14:11:40 +0000

    I have now wrote the Instructions

--- Intro.py
+++ Intro.py
@@ -5,7 +5,7 @@ def Instructions():
     requires_instruction = input("Would you like instructions? Or have you used this program before? Y/N ").strip().lower()
     #Simple if statment to check what the user answered the input with and giving appropreate responses.
     if requires_instruction == "yes" or requires_instruction == "y":
-      print("Place holder instructions")
+      print("This program is made to help you calculate which out of a set of products are best to purchase baised on a budget you will set. The program is going to ask for your budget which is how much you are willing to spend, and then you will fill in the information of products untill you have a list you are satisfied with. From there the program will analyze them and give its recomendation to you.")
       giving_instructions = False
 
     elif requires_instruction == "no" or requires_instruction == "n":
