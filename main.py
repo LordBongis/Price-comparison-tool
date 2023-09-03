65fdc8fb37505cc2d6c34674bc8425fd4be896b5
LordBongis <134457452+LordBongis@users.noreply.github.com>
2023-09-03 08:18:01 +0000

    Bug removal in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #This simply imports the other files so that they can be run through the main.py file.
 import Intro
 import Data
-import Analysation
+
 
 #This runs the files in the order the program requires ie Intro -> Data -> Analyze -> End
 Intro.Instructions()
