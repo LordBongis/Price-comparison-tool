6c5a601e0213fba1125e75b52fe9b38b74626469
LordBongis <134457452+LordBongis@users.noreply.github.com>
2023-09-03 13:03:09 +0000

    Major changes across the board, Analysation conponent has been made and nears completion ie being tested, and main has been edited so as to work with the new conponent.

--- Analysation.py
+++ Analysation.py
@@ -1,5 +1,24 @@
-
+#Importing data is a nesesity to get the product info and budget info.
 import Data
 
-def calculate_product_value():
+def calculate_product_value(products, budget):
+  #These variables are required in the calculation to get the best product and its ratio.
+  best_value_product = None
+  best_value_ratio = 0 
+  #For loop so that it can analyse all the products.
+  for product in products:
+    if product.cost <= budget:
+      value_ratio = calculate_value(product) / product.cost
+      #If statment to check the value ratio against the current best ratio and then change it if required.
+      if value_ratio > best_value_ratio:
+        best_value_ratio = value_ratio
+        best_value_product = product
+  #If statment to inform the user on the best product.
+  if best_value_product:
+    print("The best value for money is {} with a price to value raitio of {}".format(best_value_product.name, best_value_ratio))
+  else: 
+    print("No products fit within the budget. ")
+#This function simply returns the product value.
+def calculate_value(product):
+  return product.value
   
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #This simply imports the other files so that they can be run through the main.py file.
 import Intro
 import Data
-
+import Analysation
 
 #This runs the files in the order the program requires ie Intro -> Data -> Analyze -> End
 Intro.Instructions()
 Data.user_budget()
 Data.product_information()
+Analysation.calculate_product_value(Data.products, Data.budget)
